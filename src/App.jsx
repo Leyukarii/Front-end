f92ef998ae451e1d7be5b3dc9079a04bbdfc9f90
ErikArsego <erik.arsego@outlook.com>
2024-11-19 13:18:51 -0300

    FIlmes
    API filmes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,13 @@ import BuscarSessao from './pages/sessao/BuscarSessao';
 import EditarSessao from './pages/sessao/EditarSessao';
 import ExcluirSessao from './pages/sessao/ExcluirSessao';
 
+//importa filmes
+import BuscarFilme from './pages/filme/BuscarFilme'
+import CriarFilme from './pages/filme/CriarFilme'
+import EditarFilme from './pages/filme/EditarFilme'
+import ExcluirFilme from './pages/filme/ExcluirFilme'
+import ListarFilme from './pages/filme/ListarFilme'
+
 function App() {
   return (
     <Routes>
@@ -16,6 +23,12 @@ function App() {
        <Route path="/buscarSessao" element={<AppLayout><BuscarSessao /></AppLayout>} />
        <Route path="/editarSessao" element={<AppLayout><EditarSessao /></AppLayout>} />
        <Route path="/excluirSessao" element={<AppLayout><ExcluirSessao /></AppLayout>} />
+
+       <Route path="/criarFilme" element={<AppLayout><CriarFilme /></AppLayout>} />
+       <Route path="/buscarFilme" element={<AppLayout><BuscarFilme /></AppLayout>} />
+       <Route path="/editarFilme" element={<AppLayout><EditarFilme /></AppLayout>} />
+       <Route path="/excluirFilme" element={<AppLayout><ExcluirFilme /></AppLayout>} />
+       <Route path="/listarFilme" element={<AppLayout><ListarFilme /></AppLayout>} />
     </Routes>
   );
 }
